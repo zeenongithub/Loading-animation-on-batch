82baed26fad41919f06d3824755dca2ec84e7c0f
zeenongithub <121015236+zeenongithub@users.noreply.github.com>
2022-12-19 12:49:20 +0330

    Add files via upload

---
+++ Loading.bat
@@ -0,0 +1,21 @@
+@echo off
+color 0F
+title Loading
+timeout /t 1 >nul
+cls
+echo Loading.
+title Loading.
+timeout /t 1 >nul
+cls
+echo Loading..
+title Loading..
+timeout /t 1 >nul
+cls
+echo Loading...
+title Loading...
+timeout /t 1 >nul
+cls
+echo Done!
+title Done!
+pause >nul
+exit
